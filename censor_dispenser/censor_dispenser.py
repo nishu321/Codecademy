598c04e5345a0afba662517251f9861a65e66ab5
nishu321 <nissarg37@gmail.com>
2020-05-04 17:38:53 -0400

    Adding censor_dispenser project folder

---
+++ censor_dispenser/censor_dispenser.py
@@ -0,0 +1,8 @@
+# These are the emails you will be censoring. The open() function is opening the text file that the emails are contained in and the .read() method is allowing us to save their contexts to the following variables:
+email_one = open("c:/Users/nishu/Documents/Codecademy_Projects/censor_dispenser/email_one.txt", "r").read()
+email_two = open("c:/Users/nishu/Documents/Codecademy_Projects/censor_dispenser/email_two.txt", "r").read()
+email_three = open("c:/Users/nishu/Documents/Codecademy_Projects/censor_dispenser/email_three.txt", "r").read()
+email_four = open("c:/Users/nishu/Documents/Codecademy_Projects/censor_dispenser/email_four.txt", "r").read()
+
+print(email_four)
+
