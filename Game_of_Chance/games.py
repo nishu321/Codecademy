8e261e15004427ae7fd7c18bbe1a91bfc154b9f9
nishu321 <nissarg37@gmail.com>
2020-06-19 07:18:34 -0400

    Added cho_han function

--- Game_of_Chance/games.py
+++ Game_of_Chance/games.py
@@ -30,5 +30,35 @@ def head_or_tails(bet, option):
         print("New Balance: ${0}".format(money))
 
 
+def cho_han(bet, option):
+    global money
+
+    dice_one = random.randint(1, 6)
+    dice_two = random.randint(1, 6)
+
+    options = {
+        0: "Even",
+        1: "Odds"
+    }
+
+    total = dice_one + dice_two
+
+    print("Playing Cho-Han")
+    print("Current Balance: ${0}".format(money))
+    print("Bet ${0} on {1}".format(bet, option))
+
+    if options[total % 2].lower() == option.lower():
+        print("It's {0} {1}".format(options[total % 2].lower(), total))
+        print("You Won!")
+        money += bet
+        print("New Balance: ${0}".format(money))
+    else:
+        print("It's {0} {1}".format(options[total % 2].lower(), total))
+        print("You Lost!")
+        money -= bet
+        print("New Balance: ${0}".format(money))
+
+
 # Call your game of chance functions here
 head_or_tails(10, "heads")
+cho_han(10,"even")
